d7c8f0240e63e8be78890b34ff8b722b46ef5bc1
mykhailo_naumenko <74735041+mikhail2574@users.noreply.github.com>
2025-07-11 10:19:31 +0200

    created simple multithreading

--- exercises/phase-1/index.mjs
+++ exercises/phase-1/index.mjs
@@ -1,22 +1,64 @@
-import fastify from 'fastify'
-import { randomBytes } from 'node:crypto'
+import fastify from "fastify";
+import { createHash, randomBytes } from "node:crypto";
+import { isMainThread, parentPort, Worker } from "node:worker_threads";
 
-const app = fastify({ logger: process.env.VERBOSE === 'true' })
+function startWorker() {
+  parentPort.on("message", (message) => {
+    if (message?.type !== "request") {
+      return;
+    }
 
-app.get('/fast', async () => {
-  // TODO: Compute the time variable with the current Epoch Time in milliseconds
+    const bytes = randomBytes(1e9);
+    parentPort.postMessage({
+      type: "response",
+      id: message.id,
+      hash: createHash("sha256").update(bytes).digest("hex"),
+    });
+  });
+}
 
-  return { time }
-})
+function startServer() {
+  const app = fastify({ logger: process.env.VERBOSE === "true" });
+  const worker = new Worker(import.meta.filename);
 
-app.get('/slow', async () => {
-  const bytes = randomBytes(1e9)
+  let requestIndex = 0;
+  const inflights = {};
 
-  // TODO: Compute the hash variable with the SHA256 of bytes.
+  worker.on("message", (message) => {
+    if (message?.type !== "response") {
+      return;
+    }
 
-  return { hash }
-})
+    inflights[message.id](message.hash);
+  });
 
-app.listen({ port: 3000 }, () => {
-  console.log(`The server is listening at http://127.0.0.1:${app.server.address().port} ...`)
-})
+  app.get("/fast", async () => {
+    return { time: Date.now() };
+  });
+
+  app.get("/slow", async () => {
+    const id = requestIndex++;
+
+    const promise = new Promise((_resolve) => {
+      inflights[id] = _resolve;
+    });
+
+    worker.postMessage({ type: "request", id });
+
+    return { hash: await promise };
+  });
+
+  app.listen({ port: 3000 }, () => {
+    console.log(
+      `The server is listening at http://127.0.0.1:${
+        app.server.address().port
+      } ...`
+    );
+  });
+}
+
+if (isMainThread) {
+  startServer();
+} else {
+  startWorker();
+}
